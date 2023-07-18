74769076a37f16514ef4a3ab89fff416906c9a53
Benedikt Reiter <contact@benereiter.com>
2023-07-18 15:09:05 +0200

    Update query_result.ts to pass node uncopied

--- src/query_result.ts
+++ src/query_result.ts
@@ -151,7 +151,7 @@ export default class QueryResult<
                     position,
                     search
                 }),
-                node: {...node}
+                node
             };
         });
     }
